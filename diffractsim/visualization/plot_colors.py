ee7ba7f42e7e2e0aa3d4bd0dae8a7087ce86972d
Raffi Hotter <rhotter@users.noreply.github.com>
2023-03-25 11:05:05 -0400

    cleaner plotting

--- diffractsim/visualization/plot_colors.py
+++ diffractsim/visualization/plot_colors.py
@@ -12,10 +12,21 @@ All rights reserved.
 
 """
 
-def plot_colors(self, rgb, figsize=(6, 6), xlim=None, ylim=None, text = None, units = mm, dark_background = True):
+
+def plot_colors(
+    self,
+    rgb,
+    figsize=(6, 6),
+    xlim=None,
+    ylim=None,
+    text=None,
+    units=mm,
+    dark_background=True,
+):
     """visualize the diffraction pattern colors with matplotlib"""
 
     from ..util.backend_functions import backend as bd
+
     if dark_background == True:
         plt.style.use("dark_background")
     else:
@@ -27,41 +38,45 @@ def plot_colors(self, rgb, figsize=(6, 6), xlim=None, ylim=None, text = None, un
     ax = fig.add_subplot(1, 1, 1)
 
     if xlim != None:
-        ax.set_xlim(np.array(xlim)/units)
+        ax.set_xlim(np.array(xlim) / units)
 
     if ylim != None:
-        ax.set_ylim(np.array(ylim)/units)
+        ax.set_ylim(np.array(ylim) / units)
 
-    # we use mm by default
-    if units == mm:
-        ax.set_xlabel("[mm]")
-        ax.set_ylabel("[mm]")
-    elif units == um:
-        ax.set_xlabel("[um]")
-        ax.set_ylabel("[um]")
-    elif units == cm:
-        ax.set_xlabel("[cm]")
-        ax.set_ylabel("[cm]")
-    elif units == nm:
-        ax.set_xlabel("[nm]")
-        ax.set_ylabel("[nm]")
-    elif units == m:
-        ax.set_xlabel("[m]")
-        ax.set_ylabel("[m]")
+    # # we use mm by default
+    # if units == mm:
+    #     ax.set_xlabel("[mm]")
+    #     ax.set_ylabel("[mm]")
+    # elif units == um:
+    #     ax.set_xlabel("[um]")
+    #     ax.set_ylabel("[um]")
+    # elif units == cm:
+    #     ax.set_xlabel("[cm]")
+    #     ax.set_ylabel("[cm]")
+    # elif units == nm:
+    #     ax.set_xlabel("[nm]")
+    #     ax.set_ylabel("[nm]")
+    # elif units == m:
+    #     ax.set_xlabel("[m]")
+    #     ax.set_ylabel("[m]")
 
-    if text == None:
-        ax.set_title("Screen distance = " + str(self.z * 100) + " cm")
-    else: 
-        ax.set_title(text)
+    # if text == None:
+    #     ax.set_title("Screen distance = " + str(self.z * 100) + " cm")
+    # else:
+    #     ax.set_title(text)
+
+    # don't show the axes
+    ax.axis("off")
 
     im = ax.imshow(
         (rgb),
         extent=[
-            float(self.x[0] - self.dx/2) / units,
-            float(self.x[-1] + self.dx/2) / units,
-            float(self.y[0] - self.dy/2)/ units,
-            float(self.y[-1] + self.dy/2) / units,
+            float(self.x[0] - self.dx / 2) / units,
+            float(self.x[-1] + self.dx / 2) / units,
+            float(self.y[0] - self.dy / 2) / units,
+            float(self.y[-1] + self.dy / 2) / units,
         ],
-        interpolation="spline36", origin = "lower"
+        interpolation="spline36",
+        origin="lower",
     )
     plt.show()
